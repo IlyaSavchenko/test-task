ccd46821c16e2f22452f600661fdf47356b61f35
IlyaSavchenko <sav.il.yu@gmail.com>
2019-01-13 17:28:25 +0300

    create object class

---
+++ src/app/spaceships/space-ship-class.ts
@@ -0,0 +1,15 @@
+export class SpaceShipClass {
+    constructor(public id: number,
+                public model: string,
+                public img: string,
+                public type: string,
+                public typeimg: string,
+                public price: string,
+                public prod_date: string,
+                public desc: string,
+                public count_place: string,
+                public size_team: string,
+                public size_hold: string,
+                public weapon: string,
+                public avaliable: number) {}
+}
